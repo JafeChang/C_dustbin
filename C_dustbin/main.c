7b6ee6d0682914e66eb9bafb8733510e379a4479
JafeChang <bestzjf001@163.com>
2015-05-02 21:42:03 +0800

    Initial Commit

---
+++ C_dustbin/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_dustbin
+//
+//  Created by 张健飞 on 15/5/2.
+//  Copyright (c) 2015年 jafee chang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
